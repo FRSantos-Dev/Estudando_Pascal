f807a0bdc845978ef563ac29ab61b995a9fae57b
Felipe Rocha <feliperocha.s@outlook.com>
2023-11-16 20:55:35 -0300

    mudando cor de fundo daa tela

--- Aula2/Segundo.pas
+++ Aula2/Segundo.pas
@@ -6,26 +6,31 @@ var numeroInteiro : integer;
 
 Begin
     textbackground(white);
+    clrscr;
     textcolor(blue;)
     numeroInteiro := 999999999;
     Writeln('A variavel numeroInteiro vale: ', nummeroInteiro);
     readln;
     textbackground(blue);
+    clrscr;
     textcolor(green);
     numeroReal := 100.200001;
     Writeln('A variavel numeroReal vale: ', numeroReal:0:2);
     readln;
     textbackground(green);
+    clrscr;
     textcolor(red);
     numeroReal := 100.20001;
     Writeln('A variavel numeroReal vale: ', numeroReal:0:2);
     readln;
     textbackground(red);
+    clrscr;
     textcolor(yellow);
     numeroReal := 100.2001;
     Writeln('A variavel numeroReal vale: ', numeroReal:0:2);
     readln;
     textbackground(yellow);
+    clrscr;
     textcolor(white);
     numeroReal := 100.201;
     Writeln('A variavel numeroReal vale: ', numeroReal:0:2);
