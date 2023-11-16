05aaa3e87325606b73a25d61ae161676ef0da205
Felipe Rocha <feliperocha.s@outlook.com>
2023-11-16 20:36:05 -0300

    Variaveis,casas decimais e cores

---
+++ Aula2/Segundo.pas
@@ -0,0 +1,17 @@
+Program Aula2;
+
+var numeroInteiro : integer;
+    Texto         : string;
+    numeroReal    : real;
+
+Begin
+    numeroInteiro := 10;
+    Texto         := 'Aula 2';
+    numeroReal    := 10.5;
+    
+    writeln('O valor da variavel numeroInteiro é: ', numeroInteiro);
+    writeln('O valor da variavel Texto é: ', Texto);
+    writeln('O valor da variavel numeroReal é: ', numeroReal:0:2);
+    readln;
+
+End.
