eef60ffd3227c0b93de709d574e026796ea2e3b3
Felipe Rocha <feliperocha.s@outlook.com>
2023-11-20 17:37:18 -0300

    par ou impar

--- Aula4/For,While,Repeat.pas
+++ Aula4/For,While,Repeat.pas
@@ -1,17 +1,18 @@
 Program Pzim;
 
 Begin
-Nomes := '';
-i := 0;
-repeat
-    clrscr;
-    i := i + 1;
-    Writeln('Digite o ',i,'º nome: ');
-    Readln(NomeAtual);
-    Nomes := Nomes + NomeAtual + ' ';
-    
-until (NomeAtual = 'FIM');
-Writeln('Os nomes Digitados foram: ',Nomes);
-Writeln('---------------------------------');
-
+wrtieln('Digite -1 para sair do programa');
+while (n <> -1) do
+begin
+writeln('Digite um numero');
+readln(n);
+if (n <> -1) then
+begin
+if (n mod 2 = 0) then
+writeln('O numero e par')
+else
+writeln('O numero e impar');
+end;
+end;
+Write('Muito obrigado por usar o programa :)');
 End;
